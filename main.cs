8e59df4f61df1e3ea2cead3c39db6c4257c3071a
jheiser <42621294+jheiser@users.noreply.github.com>
2019-01-23 15:40:55 -0600

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    string name;
+
+    Console.WriteLine ("Please enter your name:");
+    name = Console.ReadLine();
+    Console.WriteLine("Hello " + name + "!");
+
+  }
+}
